510c9aeb6602572d04cb64e5a5dbfdf048029d0e
jward1701d <jward1701d@gmail.com>
2018-01-10 18:51:32 -0500

    setup react folder structure and created template file

---
+++ src/components/Template.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Template() {
+  return (
+    <div>
+      <div className="wrapper">
+        <header>
+          <h1>MusicList</h1>
+        </header>
+      </div>
+      <div className="content">
+                Content will go here.
+      </div>
+    </div>
+  );
+}
